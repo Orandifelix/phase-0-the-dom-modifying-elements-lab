bd74b66d9f1ef00772513357a95482f1ead83e39
Orandifelix <125347734+Orandifelix@users.noreply.github.com>
2023-03-23 16:56:26 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const container = document.querySelector('main');
+container.remove("main");
+
+const newH1 = document.createElement('h1');
+newH1.setAttribute('id', 'victory');
+document.body.appendChild(newH1);
+const newHeader = document.getElementById('victory')
+
+newHeader.textContent = ("YOUR-NAME is the champion" )
